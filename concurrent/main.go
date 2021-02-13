32c8c483d531bdd1aadc026936b7acff8e46b3e4
Hunter Hancock <dr.miles.manners.ss@gmail.com>
2021-02-12 19:31:43 -0700

    yeah, wrapping sync.WaitGroup isn't really worth it

--- concurrent/main.go
+++ concurrent/main.go
@@ -13,26 +13,3 @@ var Log *log.Logger = log.New(os.Stdout, "", 0)
 func CreateLock() *sync.Mutex {
 	return &sync.Mutex{}
 }
-
-type WaitGroup struct {
-	wg sync.WaitGroup
-}
-
-func NewWaitGroup() *WaitGroup {
-	ret := WaitGroup{}
-	ret.wg.Add(1)
-	return &ret
-}
-
-func (WaitGroup *WaitGroup) Start() {
-	WaitGroup.wg.Add(1)
-}
-
-func (WaitGroup *WaitGroup) Stop() {
-	WaitGroup.wg.Add(1)
-}
-
-func (WaitGroup *WaitGroup) Wait() {
-	WaitGroup.wg.Done()
-	WaitGroup.wg.Wait()
-}
